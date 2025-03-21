49f2fa6dabb207d4e56d34fbad63e55c1ba4e351
Simon Anders <sanders@fs.tum.de>
2025-03-21 17:46:33 +0100

    cmaes works

--- dens_est_cmaes.R
+++ dens_est_cmaes.R
@@ -1,3 +1,16 @@
+library(Rcpp)
+
+eigen_flags <- system("pkg-config --cflags eigen3", intern = TRUE)
+custom_inc  <- normalizePath("libcmaes/include")
+Sys.setenv(PKG_CXXFLAGS = paste(eigen_flags, "-I", custom_inc, "-lcmaes"))
+lib_path <- normalizePath("libcmaes/lib")
+Sys.setenv(PKG_LIBS = paste("-L", lib_path, "-lcmaes", "-Wl,-rpath", lib_path))
+
+sourceCpp("test_cmaes.cc", verbose=TRUE, rebuild=TRUE)
+test() 
+
+
+
 ## Construct test data
 n <- 1500
 s <- round( 10^rnorm( n, log10(2500), .5 ) )
@@ -8,39 +21,22 @@ hist( log10(truelambda), freq=FALSE, xlab="log10(lambda)", 30, main="", xlim=c(-
 
 ## Estimator hyperparameters
 min_mu <- 3e-5
-stepsize <- 1.1
+stepsize <- 1.13
 overlap_factor <- 1
-smoothness <- 30
 
 ## Construct density basis
 mu <- exp( seq( log(min_mu), 0, by=log(stepsize) ) )
 shape <- 30/overlap_factor / stepsize  # kappa
 scale <- mu / shape    # theta
+smoothness <- 2
 
 ## Calculate NB likelihoods
 nbl <- sapply( mu, function(mu_) dnbinom( k, mu=s*mu_, size=shape ) )
 
-## Find likelihood maximum
-map_to_simplex <- function(x) {
-   v <- 1/(1+exp(-x))
-   spi <- c( 1, cumprod(v) ) * c( 1-v, 1 ) }
+x <- do_sann( nbl )
 
-objective <- function(x) {
-   v <- 1/(1+exp(-x))
-   spi <- c( 1, cumprod(v) ) * c( 1-v, 1 )
-   -( sum( log( nbl %*% spi ) ) + 
-         sum( log( v*(1-v) ) ) +
-         sum( log(v) * (length(v) - 0:(length(v)-1) - 1) ) -
-         sum( diff(spi)^2 ) * length(nbl)*smoothness/100 ) }
-
-o <- optim( rnorm( ncol(nbl)-1 ), objective,
-   method="SANN", control=list( maxit=10000, tmax=20) )
-
-o <- optim( o$par, objective,
-   method="Nelder-Mead", control=list(maxit=10000) )
-
-## Get density estimate
-spi <- map_to_simplex( o$par )
+v <- 1/(1+exp(-x))
+spi <- c( 1, cumprod(v) ) * c( 1-v, 1 ) 
 dens_est <- data.frame(
    log10lambda = seq( -6, 0, length.out=1000 ) )
 dens_est$density <- (
@@ -49,8 +45,6 @@ dens_est$density <- (
          (10^dens_est$log10lambda)*log(10) ) 
    %*% spi )
 
-## Plot result
-#hist( log10(truelambda), freq=FALSE, xlab="log10(lambda)", 30, main="", xlim=c(-6,1) )
 xg <- seq( -6, 0, length.out=1000 )      
 lines( dens_est, col="red" )
 
--- test_cmaes.cc
+++ test_cmaes.cc
@@ -3,7 +3,6 @@
 #include <Rcpp.h>
 
 using namespace std;
-using namespace Rcpp;
 using namespace libcmaes;
 
 void transform_to_simplex(const double *vals, double *simplex_point, int len) {
@@ -21,6 +20,7 @@ double log_target_dens(const double * vals_inp, const Rcpp::NumericMatrix& lhm) 
    double ltd = 0.0;
    double vals[len-1];
    
+   R_CheckUserInterrupt();
    
    // Logistic transform
    for (int j = 0; j < len - 1; j++) {
@@ -49,6 +49,8 @@ double log_target_dens(const double * vals_inp, const Rcpp::NumericMatrix& lhm) 
       ltd += std::log(lh);
    }
    
+   // cout << ltd << endl;
+   
    return ltd;
 }
 
@@ -56,6 +58,27 @@ double log_target_dens(const double * vals_inp, const Rcpp::NumericMatrix& lhm) 
 double wrapped_log_target_dens(const Rcpp::NumericVector& vals_inp, const Rcpp::NumericMatrix& lhm) {
    return log_target_dens( vals_inp.begin(), lhm );
 }
+   
+// [[Rcpp::export]]
+Rcpp::NumericVector do_sann( const Rcpp::NumericMatrix& lhm ) {
+   int dim = lhm.ncol()-1; // problem dimensions.
+   vector<double> x0(dim,0.0);
+   double sigma = 0.1;
+   //int lambda = 100; // offsprings at each generation.
+   CMAParameters<> cmaparams(x0,sigma);
+   cmaparams.set_max_iter(100000);
+   cmaparams.set_ftolerance(1e-1);
+   //cmaparams._algo = BIPOP_CMAES;
+   libcmaes::FitFunc objective =
+      [&lhm](const double *x, const int &N) { return -log_target_dens( x, lhm ); }; 
+   CMASolutions cmasols = cmaes<>(objective, cmaparams);
+   cout << "best solution: " << cmasols << endl;
+   cout << "optimization took " << cmasols.elapsed_time() / 1000.0 << " seconds\n";
+   cout << "status: " << cmasols.run_status() << endl;
+   auto x = cmasols.best_candidate().get_x();
+   return Rcpp::NumericVector( x.begin(), x.end() );
+   
+}   
    
 libcmaes::FitFunc fsphere = [](const double *x, const int N)
 {
